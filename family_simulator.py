e663bfcb6660816453d38e54c85b0f83a39b554d
Florian Haider <149242442+FlorianHaider1@users.noreply.github.com>
2023-11-20 15:34:50 +0100

    Update family_simulator.py

---
+++ family_simulator.py
@@ -0,0 +1,49 @@
+# This Programm a simulation of a household inspired by "The Sims" to practice OOP. 
+
+# Classes and Attributes:
+# Define a base class Person with attributes like name, age, and gender.
+# Create classes Furniture and Consumable with relevant attributes (e.g., type, color for Furniture; name, expiration_date for Consumable).
+
+# Inheritance:
+# Extend the Person class to create Child, Parent, and Grandparent classes, adding or modifying attributes and methods as needed.
+
+# Encapsulation, Properties (Getter & Setter):
+# Use encapsulation in the Person class. For instance, make the age attribute private and provide a getter and setter.
+# Demonstrate a case where a setter is more appropriate (e.g., updating age) versus direct access.
+
+# Constructor, Instantiation:
+# Ensure each class has an appropriate constructor (__init__ method) to initialize its attributes.
+
+# Object Lifetime & Garbage Collector:
+# Experiment with creating and deleting objects, and observe how Python's garbage collector works.
+
+# Association and Composition:
+# Use composition to create a House class containing Person, Furniture, and Consumable objects.
+# Show association by, for example, linking Child objects with Parent objects.
+
+# Copy vs. Deepcopy, Mutable Default Arguments:
+# Demonstrate the difference between shallow and deep copies with objects.
+# Explore the mutable default argument problem and its solution.
+
+# Reflection and Serialization:
+# Use reflection to inspect objects at runtime (e.g., list all attributes of a Person).
+# Implement serialization to save and load the state of your household simulation.
+
+# Lazy Loading:
+# Implement lazy loading for a resource-intensive attribute in one of your classes.
+
+# Inheritance and Multiple Inheritance:
+# Utilize multiple inheritance carefully in your class design, if applicable.
+
+# Duck Typing and (Runtime) Polymorphism:
+# Show examples of duck typing and polymorphism in your simulation.
+
+# Dunders (__str__, __repr__, __bool__, etc.):
+# Implement these methods in your classes to define their string representation, boolean conversion, etc.
+
+# Slots:
+# Use __slots__ in a class to explicitly declare allowed attributes and understand its memory benefits.
+
+# Best Practices, Parameter Classes, and Multiple Returns:
+# Create classes to encapsulate parameters where needed.
+# Demonstrate functions/methods that return multiple values.
