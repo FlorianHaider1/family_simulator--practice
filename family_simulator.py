74d014dbfecb3dc963e22ec71e619899b30310a2
Florian Haider <149242442+FlorianHaider1@users.noreply.github.com>
2023-11-21 15:28:08 +0100

    Update family_simulator.py

--- family_simulator.py
+++ family_simulator.py
@@ -1,49 +1,23 @@
-# This Programm a simulation of a household inspired by "The Sims" to practice OOP. 
+# This Programm is household-simulation inspired by "The Sims" to practice OOP. 
+# Classes and Attributes, including constructor
+#   Base class Person with attributes like name, age, and gender.
+#   Base class Furniture: type, color   
+#   Base class Consumable: name, expiration_date 
+# Composition:  
+#   Composition: Base class house: containing Person, Furniture, and Consumable objects.
+#   Composition: create rooms in the house and purposes for the rooms
+#   Composition: link e.g. parents and children
+# x Inheritance: Extend Person class: Child, Parent, and Grandparent. Be aware of multiple inheritances.
+# x Encapsulation, Properties (Getter & Setter):  make the age attribute private and provide a getter and setter.
+# x Copy vs. Deepcopy: Use shallow and deep copies with objects.
+# x Mutable Default Arguments: Explore the mutable default argument problem and its solution.
+# x Reflection: Use reflection to inspect objects at runtime (e.g., list all attributes of a Person). 
+# Serialization: Implement serialization to save and load the state of your household simulation.
+# To Add:
+# Lazy Loading: # Implement lazy loading for a resource-intensive attribute in one of your classes.
+# Duck Typing and (Runtime) Polymorphism: Show examples of duck typing and polymorphism in your simulation.
+# Dunders (__str__, __repr__, __bool__, etc.): Implementin your classes to define their string representation, boolean conversion, etc.
+# Slots: Use __slots__ in a class to explicitly declare allowed attributes and understand its memory benefits.
+# Object Lifetime & Garbage Collector: Experiment with creating and deleting objects, and observe how Python's garbage collector works.
+# Make a usecase where set/get is better than setter/getter
 
-# Classes and Attributes:
-# Define a base class Person with attributes like name, age, and gender.
-# Create classes Furniture and Consumable with relevant attributes (e.g., type, color for Furniture; name, expiration_date for Consumable).
-
-# Inheritance:
-# Extend the Person class to create Child, Parent, and Grandparent classes, adding or modifying attributes and methods as needed.
-
-# Encapsulation, Properties (Getter & Setter):
-# Use encapsulation in the Person class. For instance, make the age attribute private and provide a getter and setter.
-# Demonstrate a case where a setter is more appropriate (e.g., updating age) versus direct access.
-
-# Constructor, Instantiation:
-# Ensure each class has an appropriate constructor (__init__ method) to initialize its attributes.
-
-# Object Lifetime & Garbage Collector:
-# Experiment with creating and deleting objects, and observe how Python's garbage collector works.
-
-# Association and Composition:
-# Use composition to create a House class containing Person, Furniture, and Consumable objects.
-# Show association by, for example, linking Child objects with Parent objects.
-
-# Copy vs. Deepcopy, Mutable Default Arguments:
-# Demonstrate the difference between shallow and deep copies with objects.
-# Explore the mutable default argument problem and its solution.
-
-# Reflection and Serialization:
-# Use reflection to inspect objects at runtime (e.g., list all attributes of a Person).
-# Implement serialization to save and load the state of your household simulation.
-
-# Lazy Loading:
-# Implement lazy loading for a resource-intensive attribute in one of your classes.
-
-# Inheritance and Multiple Inheritance:
-# Utilize multiple inheritance carefully in your class design, if applicable.
-
-# Duck Typing and (Runtime) Polymorphism:
-# Show examples of duck typing and polymorphism in your simulation.
-
-# Dunders (__str__, __repr__, __bool__, etc.):
-# Implement these methods in your classes to define their string representation, boolean conversion, etc.
-
-# Slots:
-# Use __slots__ in a class to explicitly declare allowed attributes and understand its memory benefits.
-
-# Best Practices, Parameter Classes, and Multiple Returns:
-# Create classes to encapsulate parameters where needed.
-# Demonstrate functions/methods that return multiple values.
